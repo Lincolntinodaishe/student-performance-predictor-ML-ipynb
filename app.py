8a9cf62a3a3080a948c6f94922ed8b55cbe262d8
Tinodaishe Lincoln Chitswa <tinodaishe04@gmail.com>
2025-09-25 04:11:10 -0500

    Fix slider type errors and update requirements

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ model = joblib.load('best_model.pkl')
 
 st.title("Student Performance Predictor")
 
-study_time = st.slider("Study Time (hours per day)", 0, 12.0, 2.0)
-social_media_time = st.slider("Social Media Time (hours per day)", 0, 12.0, 2.0)
+study_time = st.slider("Study Time (hours per day)", 0, 12, 2)
+social_media_time = st.slider("Social Media Time (hours per day)", 0, 12, 2)
 attendance = st.slider("Attendance Rate (%)", 0, 100, 80)
 mental_health = st.slider("Mental Health Score (1-10)", 1, 10, 5)
-sleep_hours = st.slider("Sleep Hours (per night)", 0, 12.0, 7.0)
+sleep_hours = st.slider("Sleep Hours (per night)", 0, 12, 7)
 part_time_job = st.selectbox("Part-time Job", ["No", "Yes"])
 
 ptj_encoded = 1 if part_time_job == "Yes" else 0
@@ -20,9 +20,7 @@ ptj_encoded = 1 if part_time_job == "Yes" else 0
 if st.button("Predict Performance"):
     input_data= np.array([[study_time, social_media_time, attendance, mental_health, sleep_hours, ptj_encoded]])
     prediction = model.predict(input_data)[0]
-
     prediction =  max(0, min(100, prediction))
-    
     st.success(f"Predicted Performance Score: {prediction:.2f}/100")
     
 
